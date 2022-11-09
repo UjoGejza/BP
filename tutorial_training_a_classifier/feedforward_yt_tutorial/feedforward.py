027303e2c894f096e0a0ef99fdbf908f39f15c60
Sebastián Chupáč <sebastianchupac@gmail.com>
2022-11-09 14:19:11 +0100

    Experimenting with one-hot encoding

--- tutorial_training_a_classifier/feedforward_yt_tutorial/feedforward.py
+++ tutorial_training_a_classifier/feedforward_yt_tutorial/feedforward.py
@@ -7,6 +7,7 @@ import torchvision
 import torchvision.transforms as transforms
 import matplotlib.pyplot as plt
 
+
 # device config
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
