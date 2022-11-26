884e4a62da0f71c342f4b93a75f4d805c9a31f3f
Sebastián Chupáč <sebastianchupac@gmail.com>
2022-11-26 12:52:03 +0100

    Po stretnuti

--- one-hot_encoding/dataset/dataset.py
+++ one-hot_encoding/dataset/dataset.py
@@ -26,7 +26,20 @@ class MyDataset(Dataset):
         self.f = open(file, "r", encoding="cp1252") #0x92 is a smart quote(’) of Windows-1252. It simply doesn't exist in unicode, therefore it can't be decoded.
         self.charlist = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 
                         'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ' ', ',', '.', '!', 
-                        '?', '\'', '\"', '’', '-', '+', '…', '“', '”', '(', ')', ':', '/', ';']
+                        '?', '\'', '\"', '’', '-', '+', '…', '“', '”', '(', ')', ':', '/', ';']#add uppercase
+        numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+        latin = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
+         'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+         's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+        symbols = ['!', '¡', '?', '¿', ',', '—', '.', '·', ':', ';', '\\', '_', '&', '#', '@', '(', ')', '[', ']', '{', '}', '+',
+           '-', '*', '/', '±', '=', '≠', '<', '>', '≤', '≥', 'ϵ', '∞', '%', '‰', '£', '€', '$', '§', '©', '®', '℥', "'",
+           '‘', '’', '`', '„', '“', '"', '»', '«', '›', '‹', '☞', '☜', '^', '~', '°', '˛', '†', '|', '⁂', '⊥', '¬', '¤']
+
+        czech_special = ['Á', 'Č', 'Ď', 'É', 'Ě', 'Í', 'Ň', 'Ó', 'Ř', 'Š', 'Ť', 'Ú', 'Ů', 'Ý', 'Ž',
+                 'á', 'č', 'ď', 'é', 'ě', 'í', 'ň', 'ó', 'ř', 'š', 'ť', 'ú', 'ů', 'ý', 'ž']
+        #todo refactor
         self.clear_samples = torch.zeros(1750, 50)#correct line of text
         self.dirty_samples = torch.zeros(1750, 50)#same line with typos
         self.labels = torch.zeros(1750, 50)
@@ -75,7 +88,7 @@ class MyDataset(Dataset):
         return len(self.clear_samples)
 
     def __getitem__(self, idx):
-        
+        #TODO prevod na one-hot
         return {'id': self.IDs[idx],
                 'clear_sample': self.clear_samples[idx],
                 'dirty_sample': self.dirty_samples[idx],
@@ -101,12 +114,13 @@ class NeuralNetwork(nn.Module):
         self.linear1 = nn.Linear(50, 256)
         self.relu = nn.LeakyReLU()
         self.linear2 = nn.Linear(256, 50)
+        self.sigmoid = nn.Sigmoid()
         
     def forward(self, x):
         out = self.linear1(x)
         out = self.relu(out)
         out = self.linear2(out)
-        #out = self.relu(out)
+        out = self.sigmoid(out)
 
         #Binary Activation Function
         #estimator = StraightThroughEstimator()
@@ -115,7 +129,8 @@ class NeuralNetwork(nn.Module):
         return out
 
 model = NeuralNetwork()
-loss_fn = nn.CrossEntropyLoss()
+#nn.BCEWithLogitsLoss
+loss_fn = nn.BCELoss()
 print(model.parameters())
 optimizer = torch.optim.Adam(model.parameters(), lr=0.005)
 
@@ -133,6 +148,7 @@ def train():
 
         if (epoch+1)%10==0:
             print(f'epoch {epoch+1}, step {i+1} / {n_total_steps}, loss = {loss.item():.4f}')
+
 
 train()
 
